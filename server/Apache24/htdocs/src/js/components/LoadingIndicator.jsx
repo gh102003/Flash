7b78288f1455957a7d9529e45850a8e1f2aac142
George Howarth <george@pyesmeadow.com>
2019-08-07 14:13:07 +0100

    Remove old python code

--- server/Apache24/htdocs/src/js/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function LoadingIndicator() {
-    return (
-        <div className="loading-indicator">
-            Loading... 
-        </div>
-    );
-}
--- server/Apache24/htdocs/src/js/constants.js
+++
@@ -1,6 +0,0 @@
-export const version = 0.2;
-
-export const draggableTypes = {
-    FLASHCARD: "flashcard",
-    SUBCATEGORY: "subcategory"
-};
