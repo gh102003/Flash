ecc7b6f965e14eec2db184ee55c5cbe21c8dc112
George Howarth <george@pyesmeadow.com>
2019-10-15 21:14:11 +0100

    HTTPS for backend

---
+++ nodeServer/https_server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
+const app = require("./app");
+
+const port = process.env.PORT || 444;
+
+const server = https.createServer({
+    key: fs.readFileSync("/etc/letsencrypt/live/flashapp.uk.to/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/flashapp.uk.to/fullchain.pem")
+}, app); // Set express app as request handler
+
+server.listen(port);
