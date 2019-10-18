665f0ed06a2f1500f3f1003d8146234256296274
George Howarth <george@pyesmeadow.com>
2019-10-18 12:17:24 +0100

    HTTPS changes

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 
-const port = process.env.PORT || 81;
+const port = process.env.PORT || 3001;
 
-const server = http.createServer(app); // Set express app as request handler
+//const server = http.createServer(app); // Set express app as request handler
 
-server.listen(port);
+app.listen(port); // Use app to listen to enable HTTPS
