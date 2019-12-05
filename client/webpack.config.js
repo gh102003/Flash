c952ffb161222603d71a8ffd7cd5f276d111a4f6
George Howarth <george@pyesmeadow.com>
2019-12-05 20:35:20 +0000

    Fix caching and update icons

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,7 +62,10 @@ module.exports = {
             // these options encourage the ServiceWorkers to get in there fast
             // and not allow any straggling "old" SWs to hang around
             clientsClaim: true,
-            skipWaiting: true
+            skipWaiting: true,
+            
+            navigateFallback: "/index.html"
+            // globPatterns: ["**/*.{png,js,css,html}"]
         })
     ],
     optimization: {
