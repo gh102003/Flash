16b57318209c0336c7f6ea94978892ffc08e99b4
George Howarth <george@pyesmeadow.com>
2019-12-29 19:35:54 +0000

    Version bump

--- client/src/js/constants.js
+++ client/src/js/constants.js
@@ -1,7 +1,5 @@
-export const version = "1.6.3";
+export const version = "1.6.4";
 
-// export const clientOrigin = "http://192.168.1.66";
-// export const serverOrigin = "http://192.168.1.66:3001";
 export const sourceCodeLink = "https://github.com/gh102003/Flash";
 export const dataProtectionEmail = "mailto:gh102003g@gmail.com?subject=Data Protection";
 
