ac677cdd66de3b9a5044fb2bdc6395b36fd0df59
George Howarth <george@pyesmeadow.com>
2019-12-29 19:03:23 +0000

    Disable CORS in production mode

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -32,15 +32,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json()); // JSON
 
 // Fix CORS
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+if (process.env.NODE_ENV === "development") {
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+        if (req.method === "OPTIONS") {
+            res.header("Access-Control-Allow-Methods", "POST, PATCH, DELETE, GET");
+            return res.status(200).json({});
+        }
+        next();
+    });
+}
 
 // Setup routers (middleware)
 app.use("/categories", categoryRoutes);
--- nodeServer/server.js
+++ nodeServer/server.js
@@ -4,4 +4,6 @@ const app = require("./app");
 
 const port = process.env.PORT || 3001;
 
+console.log(`API server running on port ${port} in ${process.env.NODE_ENV} mode`);
+
 app.listen(port); // Use app to listen to enable HTTPS and HTTP/2
